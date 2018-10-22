aa743583e7ed74e7cb1c2c4bc373d560026f5b28
Max <max.bergmark@gmail.com>
2018-10-22 15:31:18 +0200

    Optimized heavily

--- snippets/pytorch_mandelbrot/buddha_kernel.py
+++ snippets/pytorch_mandelbrot/buddha_kernel.py
@@ -11,6 +11,7 @@ import scipy.misc
 code = """
 #include <curand_kernel.h>
 #include <stdio.h>
+#include "/usr/local/cuda/samples/common/inc/helper_math.h"
 
 #define X_MIN -1.5f
 #define X_MAX 1.5f
@@ -29,7 +30,9 @@ code = """
 const int nstates = %(NGENERATORS)s;
 __device__ curandState_t* states[nstates];
 
-extern "C" { __global__ void init_kernel(int seed) {
+extern "C" {
+__global__
+void init_kernel(int seed) {
 
 	int idx = threadIdx.x + blockIdx.x * blockDim.x;
 
@@ -43,104 +46,118 @@ extern "C" { __global__ void init_kernel(int seed) {
 	} else {
 		printf("forbidden memory access %%d/%%d\\n", idx, nstates);
 	}
-} }
+}
+}
 
-__device__ void to_pixel(float &px, float &py, int &ix, int &iy) {
-	px -= X_MIN;
-	py -= Y_MIN;
-	px /= X_MAX - X_MIN;
-	py /= Y_MAX - Y_MIN;
-	px *= X_DIM;
-	py *= Y_DIM;
-	ix = __float2int_rd(px);
-	iy = __float2int_rd(py);
+__device__ void to_pixel(float2 &temp, int &ix, int &iy) {
+	temp.x -= X_MIN;
+	temp.y -= Y_MIN;
+	temp.x /= X_MAX - X_MIN;
+	temp.y /= Y_MAX - Y_MIN;
+	temp.x *= X_DIM;
+	temp.y *= Y_DIM;
+	ix = __float2int_rd(temp.x);
+	iy = __float2int_rd(temp.y);
 }
 
 __device__
-void write_pixel(int idx, float px, float py, int ix, int iy,
-	float4 *z, unsigned int *canvas) {
-	px = z[idx].y;
-	py = z[idx].x;
-	to_pixel(px, py, ix, iy);
+void write_pixel(float2 temp, int ix, int iy,
+	float4 z, unsigned int *canvas) {
+	temp.x = z.y;
+	temp.y = z.x;
+	to_pixel(temp, ix, iy);
 	if (0 <= ix & ix < X_DIM & 0 <= iy & iy < Y_DIM) {
-		canvas[iy*X_DIM + ix] += 1;
+		atomicAdd(&(canvas[iy*X_DIM + ix]), 1);
 	}
 }
 
 __device__
-void generate_random_complex(float real, float imag, int idx,
-	float4 *z, float *dists, unsigned int *counts) {
+void generate_random_complex(float2 temp,
+	float4 &z, float &dist, unsigned int &count) {
 
-	real *= X_MAX_SAMPLE-X_MIN_SAMPLE;
-	real += X_MIN_SAMPLE;
-	imag *= Y_MAX_SAMPLE-Y_MIN_SAMPLE;
-	imag += Y_MIN_SAMPLE;
+	temp.x *= X_MAX_SAMPLE-X_MIN_SAMPLE;
+	temp.x += X_MIN_SAMPLE;
+	temp.y *= Y_MAX_SAMPLE-Y_MIN_SAMPLE;
+	temp.y += Y_MIN_SAMPLE;
 
-	z[idx].x = real;
-	z[idx].y = imag;
-	z[idx].z = real;
-	z[idx].w = imag;
-	dists[idx] = 0;
-	counts[idx] = 0;
+	z.x = temp.x;
+	z.y = temp.y;
+	z.z = temp.x;
+	z.w = temp.y;
+	dist = 0;
+	count = 0;
 }
 
 __device__
-bool check_bulbs(int idx, float4 *z) {
-	float zw2 = z[idx].w*z[idx].w;
-	bool main_card = !(((z[idx].z-0.25)*(z[idx].z-0.25)
-		+ (zw2))*(((z[idx].z-0.25)*(z[idx].z-0.25)
-		+ (zw2))+(z[idx].z-0.25)) < 0.25* zw2);
-	bool period_2 = !((z[idx].z+1.0) * (z[idx].z+1.0) + (zw2) < 0.0625);
-	bool smaller_bulb = !((z[idx].z+1.309)*(z[idx].z+1.309) + zw2 < 0.00345);
-	bool smaller_bottom = !((z[idx].z+0.125)*(z[idx].z+0.125)
-		+ (z[idx].w-0.744)*(z[idx].w-0.744) < 0.0088);
-	bool smaller_top = !((z[idx].z+0.125)*(z[idx].z+0.125)
-		+ (z[idx].w+0.744)*(z[idx].w+0.744) < 0.0088);
+bool check_bulbs(float4 z) {
+	float zw2 = z.w*z.w;
+	bool main_card = !(((z.z-0.25)*(z.z-0.25)
+		+ (zw2))*(((z.z-0.25)*(z.z-0.25)
+		+ (zw2))+(z.z-0.25)) < 0.25* zw2);
+	bool period_2 = !((z.z+1.0) * (z.z+1.0) + (zw2) < 0.0625);
+	bool smaller_bulb = !((z.z+1.309)*(z.z+1.309) + zw2 < 0.00345);
+	bool smaller_bottom = !((z.z+0.125)*(z.z+0.125)
+		+ (z.w-0.744)*(z.w-0.744) < 0.0088);
+	bool smaller_top = !((z.z+0.125)*(z.z+0.125)
+		+ (z.w+0.744)*(z.w+0.744) < 0.0088);
 	return main_card & period_2 & smaller_bulb & smaller_bottom & smaller_top;
 }
 
-extern "C" {__global__ void buddha_kernel(unsigned int *counts, float4 *z,
-	float *dists, unsigned int *canvas) {
-	int idx = threadIdx.x + blockIdx.x * blockDim.x;
+extern "C" {
+__global__
+void buddha_kernel(unsigned int *canvas, int seed) {
+	int idx = blockIdx.x 
+		+ threadIdx.x * gridDim.x 
+		+ threadIdx.y * gridDim.x * blockDim.x;
 	int i, j, ix, iy;
-	float real, imag;//, temp0, temp1;
+	float2 temp, coord;
+	unsigned int count;
+	float4 z;
+	float dist;
+	curandState_t s;
+	curand_init(seed, idx, 0, &s);
 
-	if (idx < nstates) {
+	for (coord.x = 0; coord.x < 1; coord.x += 1/(float)blockDim.x) {
+		for (coord.y = 0; coord.y < 1; coord.y += 1/(float)blockDim.x) {
 
-		curandState_t s = *states[idx];
-		for(i = 0; i < 10000; i++) {
+			for(i = 0; i < 1; i++) {
 
-			real = curand_uniform(&s);
-			imag = curand_uniform(&s);
-			generate_random_complex(real, imag, idx, z, dists, counts);
-			if (check_bulbs(idx, z)) {
-				while (counts[idx] < ITERS & dists[idx] < 25) {
-					counts[idx]++;
-					real = z[idx].x*z[idx].x - z[idx].y*z[idx].y + z[idx].z;
-					imag = 2*z[idx].x*z[idx].y + z[idx].w;
-					z[idx].x = real;
-					z[idx].y = imag;
-					dists[idx] = z[idx].x*z[idx].x + z[idx].y*z[idx].y;
-				}
+				temp.x = curand_uniform(&s);
+				temp.y = curand_uniform(&s);
+				temp /= (float)blockDim.x;
+				temp += coord;
 
-				if (dists[idx] > 25) {
-					z[idx].x = z[idx].z;
-					z[idx].y = z[idx].w;
-					for (j = 0; j < counts[idx]+1; j++) {
-						real = z[idx].x*z[idx].x - z[idx].y*z[idx].y + z[idx].z;
-						imag = 2*z[idx].x*z[idx].y + z[idx].w;
-						z[idx].x = real;
-						z[idx].y = imag;
-						write_pixel(idx, real, imag, ix, iy, z, canvas);
+				generate_random_complex(temp, z, dist, count);
+				if (check_bulbs(z)) {
+					while (count < ITERS & dist < 25) {
+						count++;
+						temp.x = z.x*z.x - z.y*z.y + z.z;
+						temp.y = 2*z.x*z.y + z.w;
+						z.x = temp.x;
+						z.y = temp.y;
+						dist = z.x*z.x + z.y*z.y;
+					}
+
+					if (dist > 25) {
+						z.x = z.z;
+						z.y = z.w;
+						for (j = 0; j < count; j++) {
+							temp.x = z.x*z.x - z.y*z.y + z.z;
+							temp.y = 2*z.x*z.y + z.w;
+							z.x = temp.x;
+							z.y = temp.y;
+							write_pixel(temp, ix, iy, z, canvas);
+						}
 					}
 				}
 			}
+			__syncthreads();
+
 		}
-		*states[idx] = s;
-	} else {
-		printf("forbidden memory access %%d/%%d\\n", idx, nstates);
 	}
-} }
+
+}
+}
 """
 
 def print_stats(cpu_canvas, elapsed_time, x_dim, y_dim):
@@ -155,10 +172,10 @@ def print_stats(cpu_canvas, elapsed_time, x_dim, y_dim):
 	print("\tIterations per second: %.2e" % (total_iterations / (elapsed_time),))
 
 def format_and_save(cpu_canvas, x_dim, y_dim, threads, iters):
-	cpu_canvas /= np.max(cpu_canvas)
-	cpu_canvas.shape = (y_dim, x_dim)
+	cpu_canvas /= max(1, np.max(cpu_canvas))
+	# cpu_canvas.shape = (y_dim, x_dim)
 	# this just makes the color gradient more visually pleasing
-	cpu_canvas = np.minimum(20.5*cpu_canvas, cpu_canvas*.2+.8)
+	cpu_canvas = np.minimum(2.5*cpu_canvas, cpu_canvas*.2+.8)
 
 	file_name = "pycuda_%dx%d_%d_%d.png" % (x_dim, y_dim, iters, threads)
 	print("\n\tSaving %s..." % file_name)
@@ -167,8 +184,8 @@ def format_and_save(cpu_canvas, x_dim, y_dim, threads, iters):
 
 def generate_image(x_dim, y_dim, iters):
 
-	threads = 2**8
-	b_s = 2**8
+	threads = 2**4
+	b_s = 2**9
 
 	device = Device(0)
 	print("\n\t" + device.name(), "\n")
@@ -187,18 +204,18 @@ def generate_image(x_dim, y_dim, iters):
 		no_extern_c=True,
 		options=['--use_fast_math', '-O3', '--ptxas-options=-O3']
 	)
-	init_func = module.get_function("init_kernel")
+	# init_func = module.get_function("init_kernel")
 	fill_func = module.get_function("buddha_kernel")
 	seed = np.int32(np.random.randint(0, 1<<31))
-	init_func(seed, block=(b_s,1,1), grid=(threads,1,1))
+	# init_func(seed, block=(b_s,1,1), grid=(threads,1,1))
 
 	# initialize all numpy arrays
-	samples = gpuarray.zeros(threads*b_s, dtype = gpuarray.vec.float4)
-	dists = gpuarray.zeros(threads*b_s, dtype = np.float32)
-	counts = gpuarray.zeros(threads*b_s, dtype = np.uint32)
-	canvas = gpuarray.zeros(y_dim*x_dim, dtype = np.uint32)
+	# samples = gpuarray.zeros(threads*b_s, dtype = gpuarray.vec.float4)
+	# dists = gpuarray.zeros(threads*b_s, dtype = np.float32)
+	# counts = gpuarray.zeros(threads*b_s, dtype = np.uint32)
+	canvas = gpuarray.zeros((y_dim, x_dim), dtype = np.uint32)
 	t0 = time.time()
-	fill_func(counts, samples, dists, canvas, block=(b_s,1,1), grid=(threads,1,1))
+	fill_func(canvas, seed, block=(b_s,1,1), grid=(threads,1,1))
 	context.synchronize()
 	t1 = time.time()
 
