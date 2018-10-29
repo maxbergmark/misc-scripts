e12da7d69dc076920abe4701958e87364b66d4b8
Max <max.bergmark@gmail.com>
2018-10-29 17:00:25 +0100

    Implemented further coalescing

--- snippets/pytorch_mandelbrot/buddha_kernel.cu
+++ snippets/pytorch_mandelbrot/buddha_kernel.cu
@@ -23,10 +23,66 @@ __constant__ static float2 xy_scale = (float2){
 	1/(Y_MAX - Y_MIN) * Y_DIM
 };
 
-__device__ void to_pixel(float2 &temp, int2 &ixy) {
+//rotate/flip a quadrant appropriately
+__device__
+void rot(int n, int *x, int *y, int rx, int ry) {
+    if (ry == 0) {
+        if (rx == 1) {
+            *x = n-1 - *x;
+            *y = n-1 - *y;
+        }
+
+        //Swap x and y
+        int t  = *x;
+        *x = *y;
+        *y = t;
+    }
+}
+
+//convert (x,y) to d
+__device__
+int xy2d_hilbert (int n, int x, int y) {
+    int rx, ry, s, d=0;
+    for (s=n/2; s>0; s/=2) {
+        rx = (x & s) > 0;
+        ry = (y & s) > 0;
+        d += s * s * ((3 * rx) ^ ry);
+        rot(s, &x, &y, rx, ry);
+    }
+    return d;
+}
+
+//convert d to (x,y)
+__device__
+void d2xy_hilbert(int n, int d, int *x, int *y) {
+    int rx, ry, s, t=d;
+    *x = *y = 0;
+    for (s=1; s<n; s*=2) {
+        rx = 1 & (t/2);
+        ry = 1 & (t ^ rx);
+        rot(s, x, y, rx, ry);
+        *x += s * rx;
+        *y += s * ry;
+        t /= 4;
+    }
+}
+
+__device__
+int xy2d (int xd, int yd, int x, int y) {
+	int dim = 16;
+	int block = x/dim*(yd/dim) + y/dim;
+	int blockRow = x %% dim;
+	int blockCol = y %% dim;
+	return block*dim*dim + blockCol*dim + blockRow;
+//	return block;
+}
+
+
+__device__ int to_pixel(float2 &temp, int2 &ixy) {
 	temp -= xy_min;
 	temp *= xy_scale;
 	ixy = make_int2(temp);
+	return xy2d(X_DIM, Y_DIM, ixy.x, ixy.y);
 }
 
 __device__
@@ -34,9 +90,12 @@ void write_pixel(float2 temp, int2 ixy,
 	float4 z, unsigned int *canvas) {
 	temp.x = z.y;
 	temp.y = z.x;
-	to_pixel(temp, ixy);
+	int idx = to_pixel(temp, ixy);
 	if (0 <= ixy.x & ixy.x < X_DIM & 0 <= ixy.y & ixy.y < Y_DIM) {
-		atomicAdd(&(canvas[ixy.y*X_DIM + ixy.x]), 1);
+	// if (0 <= idx & idx < X_DIM*Y_DIM) {
+		// atomicAdd(&(canvas[ixy.y*X_DIM + ixy.x]), 1);
+		atomicAdd(&(canvas[idx]), 1);
+		// canvas[ixy.y*X_DIM + ixy.x] = idx;
 		// atomicAdd(&(canvas[(ixy.y+1)*X_DIM - ixy.x-1]), 1);
 	}
 }
--- snippets/pytorch_mandelbrot/buddha_kernel.py
+++ snippets/pytorch_mandelbrot/buddha_kernel.py
@@ -21,6 +21,7 @@ def print_stats(cpu_canvas, elapsed_time, x_dim, y_dim):
 
 def format_and_save(cpu_canvas, x_dim, y_dim, threads, iters):
 	cpu_canvas /= max(1, np.max(cpu_canvas))
+	cpu_canvas.shape = (y_dim, x_dim)
 	# this just makes the color gradient more visually pleasing
 	cpu_canvas = np.minimum(2.5*cpu_canvas, cpu_canvas*.2+.8)
 
@@ -29,11 +30,26 @@ def format_and_save(cpu_canvas, x_dim, y_dim, threads, iters):
 	scipy.misc.toimage(cpu_canvas, cmin=0.0, cmax=1.0).save(file_name)
 	print("\tImage saved!\n")
 
+def transform_image(canvas, x_dim, y_dim):
+	new = 0*canvas
+	for y in range(y_dim):
+		# print(y, y_dim)
+		for x in range(x_dim):
+			idx = y*x_dim + x
+			dim = 16
+			block = x//dim*(y_dim//dim) + y//dim
+			blockRow = x % dim
+			blockCol = y % dim
+			transformed = block*dim*dim + blockCol*dim + blockRow
+
+			new[idx] = canvas[transformed]
+	return new
+
 def generate_image(x_dim, y_dim, iters):
 
 	threads = 2**7
 	b_s = 2**9
-	grid_size = np.float32(1/64)
+	grid_size = np.float32(1/256)
 
 	device = Device(0)
 	print("\n\t" + device.name(), "\n")
@@ -53,7 +69,7 @@ def generate_image(x_dim, y_dim, iters):
 	)
 	fill_func = module.get_function("buddha_kernel")
 	seed = np.int32(np.random.randint(0, 1<<31))
-	canvas = gpuarray.zeros((y_dim, x_dim), dtype = np.uint32)
+	canvas = gpuarray.zeros(y_dim* x_dim, dtype = np.uint32)
 
 	t0 = time.time()
 	fill_func(canvas, seed, grid_size, block=(b_s,1,1), grid=(threads,1,1))
@@ -62,6 +78,7 @@ def generate_image(x_dim, y_dim, iters):
 
 	# fetch buffer from gpu and save as image
 	cpu_canvas = canvas.get().astype(np.float64)
+	cpu_canvas = transform_image(cpu_canvas, x_dim, y_dim)
 	context.pop()
 	elapsed_time = t1-t0
 	print_stats(cpu_canvas, elapsed_time, x_dim, y_dim)
@@ -69,7 +86,7 @@ def generate_image(x_dim, y_dim, iters):
 
 if __name__ == "__main__":
 
-	x_dim = 1440
-	y_dim = 2560
+	x_dim = 1440*1
+	y_dim = 2560*1
 	iters = 20
 	generate_image(x_dim, y_dim, iters)
